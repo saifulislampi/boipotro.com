56acba55cec5675ff8fc51d6f62871067a492347
saifulislamplabon <saifulislamplabon@gmail.com>
2016-05-13 00:21:05 +0600

    Settings Changed

--- boipotro/boipotro/settings.py
+++ boipotro/boipotro/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd Party APPS
+
+    #My Apps
+    'books',
 ]
 
 MIDDLEWARE_CLASSES = [
