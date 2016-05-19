ab4bbdf486e176ff8354c68e72967d2cf27286e1
saifulislamplabon <saifulislamplabon@gmail.com>
2016-05-19 23:15:18 +0600

    before starting coding at thursday night

--- boipotro/carts/models.py
+++ boipotro/carts/models.py
@@ -13,13 +13,16 @@ from books.models import Book
 
 
 class CartItem(models.Model):
-	cart = models.ForeignKey("Cart")
-	item = models.ForeignKey(Book)
-	quantity = models.PositiveIntegerField(default=1)
-	line_item_total = models.DecimalField(max_digits=10, decimal_places=2)
+    cart = models.ForeignKey("Cart")
+    item = models.ForeignKey(Book)
+    quantity = models.PositiveIntegerField(default=1)
+    line_item_total = models.DecimalField(max_digits=10, decimal_places=2)
 
-	def __unicode__(self):
-		return self.item.title
+    def __unicode__(self):
+        return self.item.title
+
+    def __str__(self): #python 3
+        return self.item.title
 
 	# def remove(self):
 	# 	return self.item.remove_from_cart()
@@ -45,29 +48,32 @@ post_delete.connect(cart_item_post_save_receiver, sender=CartItem)
 
 
 class Cart(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
-	items = models.ManyToManyField(Book, through=CartItem)
-	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-	subtotal = models.DecimalField(max_digits=50, decimal_places=2, default=25.00)
-	tax_percentage  = models.DecimalField(max_digits=10, decimal_places=5, default=0.085)
-	tax_total = models.DecimalField(max_digits=50, decimal_places=2, default=25.00)
-	total = models.DecimalField(max_digits=50, decimal_places=2, default=25.00)
-	# discounts
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    items = models.ManyToManyField(Book, through=CartItem)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    subtotal = models.DecimalField(max_digits=50, decimal_places=2, default=25.00)
+    tax_percentage  = models.DecimalField(max_digits=10, decimal_places=5, default=0.085)
+    tax_total = models.DecimalField(max_digits=50, decimal_places=2, default=25.00)
+    total = models.DecimalField(max_digits=50, decimal_places=2, default=25.00)
+    	# discounts
 	# shipping
 
-	def __unicode__(self):
-		return str(self.id)
+    def __unicode__(self):
+        return str(self.id)
 
-	def update_subtotal(self):
-		print("updating cart...")
-		subtotal = 0
-		items = self.cartitem_set.all()
-		for item in items:
-			subtotal += item.line_item_total
-		self.subtotal = "%.2f" %(subtotal)
-		self.save()
+    def __str__(self):
+	    return str(self.id)
 
+
+    def update_subtotal(self):
+        print("updating cart...")
+        subtotal = 0
+        items = self.cartitem_set.all()
+        for item in items:
+            subtotal += item.line_item_total
+        self.subtotal = "%.2f" %(subtotal)
+        self.save()
 
 
 
